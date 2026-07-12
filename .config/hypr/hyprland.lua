977121aa1aeedc70a91ab813c99bb1e589ab3e9c
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2026-07-12 13:28:31 +0700

    feat(hyprland): adjust hdmi monitor settings

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -3,15 +3,18 @@
 ----------------
 hl.monitor({
   output = "eDP-1",
-  mode = "1920x1200@144",
-  position = "0x0",
+  -- mode = "1920x1200@144",
+  mode = "highrr",
+  -- position = "0x0",
   scale = 1,
 })
 
 hl.monitor({
   output = "HDMI-A-1",
-  mode = "2560x1440@120",
-  position = "1920x0",
+  -- mode = "2560x1440@120",
+  mode = "highrr",
+  position = "auto",
+  cm = "dcip3",
   scale = 1,
 })
 
